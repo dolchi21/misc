4ec085c330080da0bd564d4795d0493b347c50ba
Axel Dolce <axel.dolce@gmail.com>
2022-07-14 15:58:30 -0300

    cli-ui

---
+++ cli-ui/index.js
@@ -0,0 +1,38 @@
+const blessed = require('blessed')
+const contrib = require('blessed-contrib')
+
+const screen = blessed.screen()
+screen.key(['escape', 'q', 'C-c'], (ch, key) => process.exit(0))
+
+var box = blessed.box({
+    top: 'center',
+    left: 'center',
+    width: '50%',
+    height: '50%',
+    content: 'Hello {bold}world{/bold}!',
+    tags: true,
+    border: {
+        type: 'line'
+    },
+    style: {
+        fg: 'white',
+        bg: 'magenta',
+        border: {
+            fg: '#f0f0f0'
+        },
+        hover: {
+            bg: 'green'
+        }
+    }
+});
+
+box.key('enter', function (ch, key) {
+    box.setContent('{right}Even different {black-fg}content{/black-fg}.{/right}\n');
+    box.setLine(1, 'bar');
+    box.insertLine(1, 'foo');
+    screen.render();
+});
+
+screen.append(box)
+
+screen.render()
